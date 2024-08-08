33962e8a03a9080bb2cbf1931ba0c525a5d74d86
AleCalz <calzadaloyaalejandra@gmail.com>
2024-08-08 16:15:39 -0600

    add Turns

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,48 @@
+import { useState } from "react";
 const TURNS = {
   X: "x",
   O: "o",
 };
 
-//tablero
-const board = Array(9).fill(null);
+const Square = ({ children, updateBoard, idx, isSelected }) => {
+  const classname = `square ${isSelected ? "is-selected" : ""}`;
 
-const Square = ({ children, updateBoard, idx }) => {
-  return <div className="square">{children}</div>;
+  const handleClick = () => {
+    updateBoard();
+  };
+  return (
+    <div onClick={handleClick} className={classname}>
+      {children}
+    </div>
+  );
 };
 
 function App() {
+  //estado para tablero se act
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  //nuevo estato para conocer el turno
+  const [turn, setTurn] = useState(TURNS.X);
+
+  const updateBoard = () => {
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+  };
   return (
     <main className="board">
       <h1>Juego del Gato</h1>
       <section className="game">
         {board.map((cell, idx) => {
           return (
-            <Square key={idx} idx={idx}>
-              {idx}
+            <Square key={idx} idx={idx} updateBoard={updateBoard}>
+              {board[idx]}
             </Square>
           );
         })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
   );
